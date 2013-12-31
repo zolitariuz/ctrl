d8d48a8fc43a66a91ee335ec030f09f82d966474
raul <raul@zamacona.me>
2013-12-31 11:58:21 -0600

    post types

--- themes/ctrl/inc/pages.php
+++ themes/ctrl/inc/pages.php
@@ -6,18 +6,17 @@
 
 	add_action('init', function(){
 
-
-		// CONTACTO
-		/*if( ! get_page_by_path('faq') ){
+		// PLANTA
+		if( ! get_page_by_path('planta-productiva') ){
 			$page = array(
 				'post_author' => 1,
 				'post_status' => 'publish',
-				'post_title'  => 'Frequently Asked Questions',
-				'post_name'   => 'faq'
+				'post_title'  => 'Planta Productiva',
+				'post_name'   => 'planta-productiva',
 				'post_type'   => 'page'
 			);
 			wp_insert_post( $page, true );
-		}*/
+		}
 
 
 	});
--- themes/ctrl/inc/post-types.php
+++ themes/ctrl/inc/post-types.php
@@ -7,19 +7,19 @@
 	add_action('init', function(){
 
 
-		// NOTICIAS
-		/*$labels = array(
-			'name'          => 'Noticias',
-			'singular_name' => 'Noticia',
-			'add_new'       => 'Nueva Noticia',
-			'add_new_item'  => 'Nueva Noticia',
-			'edit_item'     => 'Editar Noticia',
-			'new_item'      => 'Nueva Noticia',
+		// Clientes
+		$labels = array(
+			'name'          => 'Clientes',
+			'singular_name' => 'Cliente',
+			'add_new'       => 'Nueva Cliente',
+			'add_new_item'  => 'Nueva Cliente',
+			'edit_item'     => 'Editar Cliente',
+			'new_item'      => 'Nueva Cliente',
 			'all_items'     => 'Todas',
-			'view_item'     => 'Ver Noticia',
-			'search_items'  => 'Buscar Noticia',
+			'view_item'     => 'Ver Cliente',
+			'search_items'  => 'Buscar Cliente',
 			'not_found'     => 'No se encontro',
-			'menu_name'     => 'Noticias'
+			'menu_name'     => 'Clientes'
 		);
 
 		$args = array(
@@ -29,7 +29,7 @@
 			'show_ui'            => true,
 			'show_in_menu'       => true,
 			'query_var'          => true,
-			'rewrite'            => array( 'slug' => 'noticias' ),
+			'rewrite'            => array( 'slug' => 'clientes' ),
 			'capability_type'    => 'post',
 			'has_archive'        => true,
 			'hierarchical'       => false,
@@ -37,6 +37,102 @@
 			'taxonomies'         => array( 'category' ),
 			'supports'           => array( 'title', 'editor', 'thumbnail' )
 		);
-		register_post_type( 'noticia', $args );*/
+		register_post_type( 'cliente', $args );
+
+		// Servicios
+		$labels = array(
+			'name'          => 'Servicios',
+			'singular_name' => 'Servicio',
+			'add_new'       => 'Nueva Servicio',
+			'add_new_item'  => 'Nueva Servicio',
+			'edit_item'     => 'Editar Servicio',
+			'new_item'      => 'Nueva Servicio',
+			'all_items'     => 'Todas',
+			'view_item'     => 'Ver Servicio',
+			'search_items'  => 'Buscar Servicio',
+			'not_found'     => 'No se encontro',
+			'menu_name'     => 'Servicios'
+		);
+
+		$args = array(
+			'labels'             => $labels,
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'servicios' ),
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'hierarchical'       => false,
+			'menu_position'      => 6,
+			'taxonomies'         => array( 'category' ),
+			'supports'           => array( 'title', 'editor', 'thumbnail' )
+		);
+		register_post_type( 'servicio', $args );
+
+		// Nosotros
+		$labels = array(
+			'name'          => 'Nosotros',
+			'singular_name' => 'Nosotros',
+			'add_new'       => 'Nueva Nosotros',
+			'add_new_item'  => 'Nueva Nosotros',
+			'edit_item'     => 'Editar Nosotros',
+			'new_item'      => 'Nueva Nosotros',
+			'all_items'     => 'Todas',
+			'view_item'     => 'Ver Nosotros',
+			'search_items'  => 'Buscar Nosotros',
+			'not_found'     => 'No se encontro',
+			'menu_name'     => 'Nosotros'
+		);
+
+		$args = array(
+			'labels'             => $labels,
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'nosotros' ),
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'hierarchical'       => false,
+			'menu_position'      => 6,
+			'taxonomies'         => array( 'category' ),
+			'supports'           => array( 'title', 'editor', 'thumbnail' )
+		);
+		register_post_type( 'nosotros', $args );
+
+		// Covers
+		$labels = array(
+			'name'          => 'Covers',
+			'singular_name' => 'Cover',
+			'add_new'       => 'Nueva Cover',
+			'add_new_item'  => 'Nueva Cover',
+			'edit_item'     => 'Editar Cover',
+			'new_item'      => 'Nueva Cover',
+			'all_items'     => 'Todas',
+			'view_item'     => 'Ver Cover',
+			'search_items'  => 'Buscar Cover',
+			'not_found'     => 'No se encontro',
+			'menu_name'     => 'Covers'
+		);
+
+		$args = array(
+			'labels'             => $labels,
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'covers' ),
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'hierarchical'       => false,
+			'menu_position'      => 6,
+			'taxonomies'         => array( 'category' ),
+			'supports'           => array( 'title', 'editor', 'thumbnail' )
+		);
+		register_post_type( 'cover', $args );
 
 	});
